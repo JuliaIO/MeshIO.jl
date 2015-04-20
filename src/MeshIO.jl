587429603b5ffe7e0a205fd6193a1e67bb64fa8d
Simon Danisch <sdanisch@gmail.com>
2015-04-20 18:08:26 +0200

    added some datatypes

--- src/MeshIO.jl
+++ src/MeshIO.jl
@@ -4,5 +4,22 @@ import FileIO: File, @file_str
 using GeometryTypes, ImageIO
 include("types.jl")
 
+export Mesh
+
+
+export HomogenousAttributes
+export UVAttribute
+export UVNormalAttribute
+export NormalAttribute
+
+export vertices
+export faces
+export attributes
+
+
+typealias UVMesh{TV, TF} Mesh{Point3{TV}, Triangle{TF}, UVAttribute{TV}}
+typealias GLUVMesh UVMesh{Float32, Uint32}
+export UVMesh
+export GLUVMesh
 
 end # module
--- src/types.jl
+++ src/types.jl
@@ -2,8 +2,8 @@
 #Started accumulating all the different mesh types and attributes. 
 
 abstract MeshAttribute
-immutable Plain <: MeshAttribute end
-const PLAIN = Plain()
+immutable PlainMesh <: MeshAttribute end
+const PLAIN = PlainMesh()
 
 
 type Textured{N, ImageType <: Image} <: MeshAttribute
@@ -11,17 +11,40 @@ type Textured{N, ImageType <: Image} <: MeshAttribute
     textures::NTuple{N, ImageType}
     meta::Dict{Symbol, Any} # Description of what the texture does?
 end
+
+abstract HomogenousAttributes{Attributes} <: MeshAttribute
+
+immutable UVAttribute{T} <: HomogenousAttributes{(UV{T},)}
+    uv::Vector{UV{T}}
+end
+immutable UVNormalAttribute{TexCoordinate, NORMAL} <: HomogenousAttributes{(TexCoordinate, NORMAL)}
+    uv::Vector{TexCoordinate}
+    normal::Vector{NORMAL}
+end
+
+immutable NormalAttribute{NORMAL} <: HomogenousAttributes{(NORMAL,)}
+    normal::Vector{NORMAL}
+end
 type Mesh{V, F, A <: MeshAttribute}
     vertices    ::Vector{V}
     faces       ::Vector{F}
     attributes  ::A
 end
+
+attributes{HMA <: HomogenousAttributes}(m::Type{HMA}) = isleaftype(HMA) ? attributes(super(HMA)) : HMA.parameters[1]
+attributes(m::Mesh) = m.attributes
+attributes{M <: Mesh}(m::Type{M}) = M.parameters[3]
+
+
+
+
 Mesh(v,f, a=PLAIN) = Mesh(v,f, a)
 
+vertices{M <: Mesh}(m::Type{M}) = M.parameters[1]
 vertices(m::Mesh)   = m.vertices
 faces(m::Mesh)      = m.faces
 
-function Base.show(io::IO, m::Mesh{V, F, A})
+function Base.show{V, F, A}(io::IO, m::Mesh{V, F, A})
     println(io, "vertices: ", V, " length: ", length(vertices(m)))
     println(io, "faces: ", F, " length: ", length(faces(m)))
     println(io, m.attributes)
