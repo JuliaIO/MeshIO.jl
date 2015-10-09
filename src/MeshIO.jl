1415dfda05d6efe21ad4c982489036fa332c2c93
Simon Danisch <sdanisch@gmail.com>
2015-10-09 20:39:38 +0200

    fixes for FileIO api change
    FileIO imports IO libraries now!

--- src/MeshIO.jl
+++ src/MeshIO.jl
@@ -4,23 +4,12 @@ module MeshIO
 using GeometryTypes
 using ColorTypes
 using Compat
-using FileIO
+import FileIO
 
-import FileIO: load, save
+import FileIO: DataFormat, @format_str, Stream, File, filename, stream, skipmagic
 import Base: writemime, call
 
-for format in [format"OBJ", format"PLY_ASCII", format"PLY_BINARY", format"STL_ASCII", format"STL_BINARY", format"OFF", format"2DM"]
-	eval(quote
-		load(fn::File{$format}, MeshType=GLNormalMesh) = open(fn) do s
-			skipmagic(s)
-	    	load(s, MeshType)
-		end
-		save(fn::File{$format}, msh::AbstractMesh) = open(fn, "w") do s
-	    	save(s, msh)
-		end
-	end)
-end
-call{M <: AbstractMesh}(m::Type{M}, f::AbstractString) = load(f, m)
+
 
 include("io/off.jl")
 include("io/ply.jl")
@@ -28,5 +17,14 @@ include("io/stl.jl")
 include("io/obj.jl")
 include("io/2dm.jl")
 
+load{format}(fn::File{format}, MeshType=GLNormalMesh) = open(fn) do s
+	skipmagic(s)
+	load(s, MeshType)
+end
+save{format}(fn::File{format}, msh::AbstractMesh) = open(fn, "w") do s
+	save(s, msh)
+end
+
+call{M <: AbstractMesh}(m::Type{M}, f::AbstractString) = FileIO.load(f, m)
 
 end # module
