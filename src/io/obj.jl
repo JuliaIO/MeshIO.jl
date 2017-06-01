6e30cfbe0913c23f1d0ab1c000c2a5e17e7bba0a
Robin Deits <robin.deits@gmail.com>
2017-06-01 15:42:35 -0400

    more v0.6 fixes

--- src/io/obj.jl
+++ src/io/obj.jl
@@ -14,19 +14,19 @@ function load{MT <: AbstractMesh}(io::Stream{format"OBJ"}, MeshType::Type{MT}=GL
     for line in eachline(io)
         # read a line, remove newline and leading/trailing whitespaces
         line = strip(chomp(line))
-        !isvalid(line) && error("non valid ascii in obj")
+        !isascii(line) && error("non valid ascii in obj")
 
-        if !startswith(line, "#") && !isempty(line) && !iscntrl(line) #ignore comments
+        if !startswith(line, "#") && !isempty(line) && !all(iscntrl, line) #ignore comments
             lines   = split(line)
             command = shift!(lines) #first is the command, rest the data
 
             if "v" == command # mesh always has vertices
-                push!(v, Point{3, Float32}(lines)) # should automatically convert to the right type in vertices(mesh)
+                push!(v, Point{3, Float32}(parse.(Float32, lines))) # should automatically convert to the right type in vertices(mesh)
             elseif "vn" == command && hasnormals(MT)
-                push!(n, Normal{3, Float32}(lines))
+                push!(n, Normal{3, Float32}(parse.(Float32, lines)))
             elseif "vt" == command && hastexturecoordinates(MT)
                 length(lines) == 2 && push!(lines, "0.0") # length can be two, but obj normaly does three coordinates with the third equals 0.
-                push!(uv, UVW{Float32}(lines))
+                push!(uv, UVW{Float32}(parse.(Float32, lines)))
             elseif "f" == command #mesh always has faces
                 if any(x->contains(x, "/"), lines)
                     fs = process_face_uv_or_normal(lines)
@@ -59,25 +59,14 @@ function load{MT <: AbstractMesh}(io::Stream{format"OBJ"}, MeshType::Type{MT}=GL
     return MT(HomogenousMesh(attributes))
 end
 
-
-immutable SplitFunctor
-    seperator::String
-end
-(s::SplitFunctor)(array) = split(array, s.seperator)
-
 # of form "f v1 v2 v3 ....""
 process_face{S <: AbstractString}(lines::Vector{S}) = (lines,) # just put it in the same format as the others
 # of form "f v1//vn1 v2//vn2 v3//vn3 ..."
-process_face_normal{S <: AbstractString}(lines::Vector{S}) = map(SplitFunctor("//"), lines)
+process_face_normal{S <: AbstractString}(lines::Vector{S}) = split.(lines, "//")
 # of form "f v1/vt1 v2/vt2 v3/vt3 ..." or of form "f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3 ...."
-process_face_uv_or_normal{S <: AbstractString}(lines::Vector{S}) = map(SplitFunctor("/"), lines)
-
-immutable ParseFunctor{T}
-    T::Type{T}
-end
-(::ParseFunctor{T}){T}(x) = parse(T, x)
+process_face_uv_or_normal{S <: AbstractString}(lines::Vector{S}) = split.(lines, '/')
 
 function triangulated_faces{Tf}(::Type{Tf}, vertex_indices::Vector{<:AbstractString})
-    poly_face = Face{length(vertex_indices), UInt32}(map(ParseFunctor(UInt32), vertex_indices))
+    poly_face = Face{length(vertex_indices), UInt32}(parse.(UInt32, vertex_indices))
     decompose(Tf, poly_face)
 end
