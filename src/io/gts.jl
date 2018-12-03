96ada20c5ddb4da29670f60c487132a8709d150a
Simon Christ <SimonChrist@gmx.de>
2018-12-03 14:42:59 +0100

    successive read of .gts-files

--- src/io/gts.jl
+++ src/io/gts.jl
@@ -15,15 +15,32 @@ end
 function load( st::Stream{format"GTS"}, MeshType=GLNormalMesh )
     io = stream(st)
     head = readline( io )
-    body = readlines( io )
     FT = facetype(MeshType)
     VT = vertextype(MeshType)
 
     nVertices, nEdges, nFacets = parseGtsLine( head, Tuple{Int,Int,Int} )
+    iV = iE = iF = 1
+    vertices = Vector{VT}(undef, nVertices)
+    edges = Vector{Vector{Int}}(undef, nEdges)
+    facets = Vector{Vector{Int}}(undef, nFacets)
+    for full_line::String in eachline(io)
+        # read a line, remove newline and leading/trailing whitespaces
+        line = strip(chomp(full_line))
+        !isascii(line) && error("non valid ascii in obj")
 
-    vertices = parseGtsLine.( body[1:nVertices], VT )
-    edges = parseGtsLine.( body[nVertices+1:nVertices+nEdges], Array{Int} )
-    facets = parseGtsLine.( body[nVertices+nEdges+1:end], Array{Int} )
+        if !startswith(line, "#") && !isempty(line) && !all(iscntrl, line) #ignore comments
+            if iV <= nVertices
+                vertices[iV] = parseGtsLine( line, VT )
+                iV += 1
+            elseif iV > nVertices && iE <= nEdges
+                edges[iE] = parseGtsLine( line, Array{Int} )
+                iE += 1
+            elseif iE > nEdges && iF <= nFacets
+                facets[iF] = parseGtsLine( line, Array{Int} )
+                iF += 1
+            end # if
+        end # if
+    end # for
     faces = [ FT( union( edges[facets[i][1]], edges[facets[i][2]], edges[facets[i][3]] ) ) for i in 1:length(facets) ]  # orientation not guaranteed
     return MeshType( vertices, faces )
 end
